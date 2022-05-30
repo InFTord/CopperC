ea8a7fa480181e460b122ae7b970d0a9dc7a5b38
InFTord <arkaz2008@gmail.com>
2022-05-30 13:24:21 +0500

    lalalalala

---
+++ Copper/Copper.cs
@@ -0,0 +1,37 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Copper;
+
+internal sealed class Program
+{
+	private ProgramConfig Config { get; }
+	public DiscordClient Discord { get; }
+	private CommandsNextExtension CommandsService { get; }
+
+	public Program(ProgramConfig cfg, int shardId)
+	{
+		Config = cfg;
+
+		var dcfg = new DiscordConfiguration
+		{
+			Token = Config.Token,
+			TokenType = TokenType.Bot,
+			Intents = DiscordIntents.All,
+			MinimumLogLevel = LogLevel.Trace,
+			ShardId = shardId,
+			ShardCount = Config.ShardCount
+		};
+		Discord = new DiscordClient(dcfg);
+		
+		var cncfg = new CommandsNext
+	}
+
+	public async Task RunAsync()
+	{
+		var act = new DiscordActivity("balls", ActivityType.ListeningTo);
+		await Discord.ConnectAsync(act, UserStatus.DoNotDisturb).ConfigureAwait(false);
+	}
+}
