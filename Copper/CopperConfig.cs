e46f3747ffa890455cd48a3cc007c3172feaf15d
InFTord <arkaz2008@gmail.com>
2022-05-30 13:26:33 +0500

    hello

---
+++ Copper/CopperConfig.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Copper;
+
+public sealed class ProgramConfig
+{
+	[JsonProperty("token")]
+	public string Token { get; private set; } = "OTY5ODQyNTIwODA2ODA1NTA0.GkyNww.EK6OzMdRSvfJEYeQKZRfoMX2vFATVdUzByukAA";
+
+	[JsonProperty("prefixes")] public string[] Prefixes { get; private set; } = { "c!", "c+" };
+
+	[JsonProperty("shardcount")] public int ShardCount { get; private set; } = 1;
+}
